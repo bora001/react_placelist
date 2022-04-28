0825c256600fa0acd0c4b2f15f2a1e41bd542faa
unknown <bora.vancouver@gmail.com>
2022-04-28 12:48:37 +0900

    post place

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getDatabase, ref as dataRef, push } from "firebase/database";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const Post = () => {
+  const [inputData, setInputData] = useState({});
+  const storage = getStorage();
+  const userInfo = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  const addImg = (e) => {
+    const { name, value } = e.target;
+    if (e.target.files) {
+      let reader = new FileReader();
+      let file = e.target.files[0];
+      reader.onloadend = () => {
+        setInputData(() => ({
+          ...inputData,
+          file,
+          img: reader.result,
+        }));
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setInputData((inputData) => ({ ...inputData, [name]: value }));
+    }
+  };
+
+  const postData = (e) => {
+    e.preventDefault();
+    if (userInfo.userUid) {
+      const n = Math.random().toString(36).slice(2);
+      const id = n + n + ".";
+      const type = inputData.file.name.split(".")[1];
+      const storageRef = ref(storage, id + type);
+      // const storageRef = ref(storage, inputData.file.name);
+      uploadBytes(storageRef, inputData.file);
+      getDownloadURL(ref(storage, inputData.file.name)).then((url) => {
+        console.log(url);
+        const db = getDatabase();
+        push(dataRef(db, "Place"), {
+          ...inputData,
+          img: url,
+          user: userInfo.userUid,
+        });
+      });
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <section className="section_new">
-      <form
-        className="form_new form_box"
-        method="post"
-        onsubmit="return false;"
-        enctype="multipart/form-data"
-      >
+      <form className="form_new form_box" onChange={addImg} onSubmit={postData}>
         <h2>New PlaceList</h2>
         <input type="text" placeholder="Name" name="name" required />
         <input type="text" placeholder="Location" name="location" required />
         <div className="pre_img"></div>
-        <label for="input_img">Image</label>
-        <input type="file" id="input_img" name="img" required />
+        <label htmlFor="input_img">Image</label>
+        <input
+          type="file"
+          id="input_img"
+          name="img"
+          accept="image/*"
+          required
+        />
+        {inputData.img && <img src={inputData.img} />}
         <input
           type="submit"
           className="btn_submit"
